a866e4b06a58751564358a26adaeb4efb5951384
eman hamdy <e.hamdy01090925920@gmail.com>
2020-08-15 05:32:11 +0300

    first commit

---
+++ resources/assets/js/bootstrap.js
@@ -0,0 +1,30 @@
+
+window._ = require('lodash');
+
+try {
+    window.$ = window.jQuery = require('jquery');
+    // require('bootstrap-sass');
+} catch (e) {}
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+// import Echo from 'laravel-echo'
+
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: 'your-pusher-key',
+//     cluster: 'mt1',
+//     encrypted: true
+// });
