d7c90097c7edb124c5ed007aef457d6ebf48f215
emanhamdy-a <0e0m0a0n0@gmail.com>
2020-08-15 21:55:12 +0300

    two

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="panel column is-offset-2 is-8">
-    <p class="panel-heading"style="position:relative;">
+    <p class="panel-heading" style="position:relative;">
       Vue js Phonebook
       <button class="button is-link is-outlined" style="position:absolute;top:8px;right:1px;">
         Add new
@@ -11,14 +11,14 @@
       <span class="column is-9">
         marksheet
       </span>
-      <span class="has-text-danger column is-1 panel-icon">
-        <i class="fa fa-trash" aria-hidden="true"></i>
+      <span class="column is-1 panel-icon">
+        <i class="has-text-danger fa fa-trash" aria-hidden="true"></i>
       </span>
-      <span class="has-text-info column is-1 panel-icon">
-        <i class="fa fa-edit" aria-hidden="true"></i>
+      <span class="column is-1 panel-icon">
+        <i class="has-text-info fa fa-edit" aria-hidden="true"></i>
       </span>
-      <span class="has-text-primary column is-1 panel-icon">
-        <i class="fa fa-eye" aria-hidden="true"></i>
+      <span class="column is-1 panel-icon">
+        <i class="has-text-primary fa fa-eye" aria-hidden="true"></i>
       </span>
     </a>
   </nav>
